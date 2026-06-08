027d675f768218bf9b690ba50f4a36e0e19b7bf3
mhassanrao909-bit <mhassanrao909@gmail.com>
2026-06-08 23:10:36 +0500

    Add files via upload

---
+++ OOP lab 4/OOP lab4 t1/OOP lab4 t1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab4_t1
+{
+    internal class Program
+    {
+        static int add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = add(num1, num2);
+
+            Console.WriteLine("Sum = " + result);
+           
+        
+        }
+    }
+}
---
+++ OOP lab 4/OOP lab4 t2/OOP lab4 t2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab4_t2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "data.txt";
+
+            if (File.Exists(path))
+            {
+                StreamReader file = new StreamReader(path);
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+                file.Close();
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+            }
+        }
+    }
+}
